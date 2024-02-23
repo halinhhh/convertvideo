31f5053ac574090709f4a5beec0491b9fc2cb1d5
halinhhh <lnguyenvuha.02@gmail.com>
2024-02-23 15:26:35 +0700

    add bitrate

--- convert3.py
+++ convert3.py
@@ -5,14 +5,15 @@ def binary_string_to_binary(binary_string):
     binary_data = bytes(int(binary_string[i:i+8], 2) for i in range(0, len(binary_string), 8))
     return binary_data
 
-def binary_to_video(binary_data, output_video_path, width, height, duration, audio_channels):
+def binary_to_video(binary_data, output_video_path, width, height, duration, audio_channels, bitrate):
     # Use ffmpeg to write binary data to a video file
     command = [
-        '/opt/homebrew/bin/ffmpeg',  # Adjust this path to the location of your ffmpeg executable
+        '/opt/homebrew/bin/ffmpeg',    # Adjust this path to the location of your ffmpeg executable
         '-f', 'h264',  
         '-i', '-',  
         '-vf', f'scale={width}:{height}',  
-        '-t', duration,  # Adjust the format to HH:MM:SS
+        '-b:v', bitrate,  # Adjust bitrate
+        '-t', duration,   # Adjust the format to HH:MM:SS
         '-ac', audio_channels,  
         output_video_path
     ]
@@ -26,6 +27,7 @@ def binary_to_video(binary_data, output_video_path, width, height, duration, aud
 def main():
     input_file_path = '/Users/linhha/C++/video_binary.txt'
     output_video_path = 'reconstructed_video2.mp4'
+    bitrate = '500k'  
 
     try:
         with open(input_file_path, 'r') as file:
@@ -38,7 +40,7 @@ def main():
         duration = '00:00:07'
         audio_channels = '2'
 
-        binary_to_video(binary_data, output_video_path, width, height, duration, audio_channels)
+        binary_to_video(binary_data, output_video_path, width, height, duration, audio_channels, bitrate)
         print(f"Video reconstructed and saved to {output_video_path}")
     except Exception as e:
         print(f"Error: {e}")
